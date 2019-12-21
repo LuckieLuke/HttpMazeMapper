76e2926b6bfbbd4f2ed397bbd2d31a7dee89be98
LuckieLuke <luk19984@gmail.com>
2019-12-21 20:00:02 +0100

    parser done i guess

--- src/Main.java
+++ src/Main.java
@@ -3,10 +3,21 @@ public class Main {
     private static final int mapNumber = 1;
 
     public static void main(String[] args) {
+        Parser p = new Parser();
+        String s = "10x5";
+        int[] size = p.parseSize(s);
 
+        for(int i: size)
+            System.out.println(i);
+
+        String pos = "\"down\": \"+\",\n \"left\": \"0\",\n \"right\": \"+\",\n \"up\": \"0\",\n ";
+        boolean[] posibilities = p.parsePossibilities(pos);
+
+        for(boolean b: posibilities)
+            System.out.println(b);
     }
 
-    public static void save(Maze maze, String path) { //jeśli nie będziemy tu nic do końca roboty dopisywać to trza to wrzucić do maina i tyle
+    public static void save(Maze maze, String path) { //jeśli nie będziemy tu nic do końca roboty dopisywać to trza to wrzucić do maina i wsio
         MazeFile.saveToFile(maze, path);
     }
 
--- src/Parser.java
+++ src/Parser.java
@@ -1,14 +1,54 @@
 public class Parser {
 
     public boolean[] parsePossibilities(String body) { //bool[4] ← przygotowuje tablicę z body
-        return null;
+        boolean[] pos = new boolean[4];
+
+        for(boolean b: pos)
+            b = false;
+
+        int directions = 0;
+        for(int i = 0; i < body.length(); i++) {
+            if(body.charAt(i) == ':') {
+                boolean pass = (body.charAt(i+3) == '0');
+                switch (directions) {
+                    case 0:
+                        pos[3] = pass;
+                        break;
+                    case 1:
+                        pos[0] = pass;
+                        break;
+                    case 2:
+                        pos[2] = pass;
+                        break;
+                    case 3:
+                        pos[1] = pass;
+                        break;
+                }
+                directions++;
+            }
+        }
+
+        return pos;
     }
 
     public int[] parseSize(String body) {
-        return null;
+        String[] sizeString = body.split("x");
+        int[] size = new int[sizeString.length];
+
+        for(int i = 0; i < size.length; i++)
+            size[i] = Integer.parseInt(sizeString[i]);
+
+        return size;
     }
 
     public int parseMoves(String body) {
-        return 0;
+        int moves = 0;
+        try {
+            moves = Integer.parseInt(body);
+        } catch (NumberFormatException e) {
+            System.out.println("Cannot format to Integer!");
+        }
+
+        return moves;
     }
 }
